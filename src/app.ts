f76562cc35faba8881098068c00e2df0cebd23ab
mukto110 <merajhossainmukto@gmail.com>
2025-01-30 00:35:11 +0600

    chore: basic route setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
+import { MovieRoutes } from "./modules/movies/movie.route";
 const app = express();
+
+app.use("/api/movies", MovieRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
