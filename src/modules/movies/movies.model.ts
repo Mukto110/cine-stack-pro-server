af0c2867dc52372025a17e90a19088f2325b4ed2
mukto110 <merajhossainmukto@gmail.com>
2025-01-29 12:28:28 +0600

    chore: implement movies interface and model

---
+++ src/modules/movies/movies.model.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+import { TMovie, TReview } from "./movies.interface";
+
+const reviewSchema = new Schema<TReview>({
+  email: { type: String, required: true },
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  comment: { type: String, required: true },
+});
+
+const movieSchema = new Schema<TMovie>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  releaseDate: { type: String, required: true },
+  genre: { type: String, required: true },
+  isDeleted: { type: Boolean, default: false },
+  viewCount: { type: Number, required: true, default: 0 },
+  reviews: { type: [reviewSchema], default: [] },
+  slug: { type: String, required: true, unique: true },
+});
+
+export const Movie = model<TMovie>("Movie", movieSchema);
